394afd2585d1c7adfabe560513c5bb037b4f514c
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-05-19 01:07:34 +0200

    feat(application): add PaginatedUserResponse DTO
    Added a new `PaginatedUserResponse` class in the `Lms.Auth.Application.DTOs` namespace.
    This class includes properties for paginated user data:
    - `Users`: A collection of `UserResponse` objects.
    - `PageIndex`, `TotalPages`, `TotalCount`: Pagination metadata.
    - `HasPreviousPage`, `HasNextPage`: Indicators for navigation.

---
+++ Lms.Auth/Lms.Auth.Application/DTOs/PaginatedUserResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Lms.Auth.Application.DTOs;
+
+public class PaginatedUserResponse
+{
+    public IEnumerable<UserResponse> Users { get; set; } = Enumerable.Empty<UserResponse>();
+    public int PageIndex { get; set; }
+    public int TotalPages { get; set; }
+    public int TotalCount { get; set; }
+    public bool HasPreviousPage { get; set; }
+    public bool HasNextPage { get; set; }
+}
