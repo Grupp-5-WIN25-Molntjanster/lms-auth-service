2d2da8fd5b77b83c0c1b4d24193d37c708ff11ff
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-05-19 01:11:49 +0200

    feat(application): add IApplicationDbContext interface
    Introduced the `IApplicationDbContext` interface in the `Lms.Auth.Application.Interfaces` namespace.
    
    - Added `DbSet<User>` and `DbSet<RefreshToken>` properties to represent database sets for `User` and `RefreshToken` entities.
    - Added `SaveChangesAsync` method for asynchronous database operations.
    - Included necessary `using` directives for `Lms.Auth.Domain.Entities` and `Microsoft.EntityFrameworkCore`.

---
+++ Lms.Auth/Lms.Auth.Application/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Lms.Auth.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lms.Auth.Application.Interfaces;
+
+public interface IApplicationDbContext
+{
+    DbSet<User> Users { get; }
+    DbSet<RefreshToken> RefreshTokens { get; }
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
