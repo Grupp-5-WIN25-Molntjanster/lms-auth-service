9951a82a6a7b0cc9591e9d8b03d44955e42e70eb
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-05-19 01:23:12 +0200

    feat(infrastructure): add PasswordHasher implementation
    Added a new PasswordHasher class in the Lms.Auth.Infrastructure.Security namespace, implementing the IPasswordHasher interface. The class provides methods for hashing and verifying passwords using BCrypt. Included the Lms.Auth.Application.Interfaces namespace for interface reference.

---
+++ Lms.Auth/Lms.Auth.Infrastructure/Security/PasswordHasher.cs
@@ -0,0 +1,9 @@
+﻿using Lms.Auth.Application.Interfaces;
+
+namespace Lms.Auth.Infrastructure.Security;
+
+public class PasswordHasher : IPasswordHasher
+{
+    public string Hash(string password) => BCrypt.Net.BCrypt.HashPassword(password);
+    public bool Verify(string password, string hash) => BCrypt.Net.BCrypt.Verify(password, hash);
+}
