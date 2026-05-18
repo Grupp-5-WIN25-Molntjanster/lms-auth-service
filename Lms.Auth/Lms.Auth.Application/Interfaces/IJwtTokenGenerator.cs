ccfdaf704c46166fe5d2802b123dc0b4ba5ce8e6
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-05-19 01:13:19 +0200

    feat(application): add IJwtTokenGenerator interface
    Introduced the `IJwtTokenGenerator` interface in the `Lms.Auth.Application.Interfaces` namespace.
    
    - Added methods for generating access and refresh tokens.
    - Included properties for token expiration configurations.
    - Added `using` directive for `Lms.Auth.Domain.Entities` to use the `User` entity.

---
+++ Lms.Auth/Lms.Auth.Application/Interfaces/IJwtTokenGenerator.cs
@@ -0,0 +1,11 @@
+﻿using Lms.Auth.Domain.Entities;
+
+namespace Lms.Auth.Application.Interfaces;
+
+public interface IJwtTokenGenerator
+{
+    string GenerateAccessToken(User user);
+    string GenerateRefreshToken();
+    int AccessTokenExpirationMinutes { get; }
+    int RefreshTokenExpirationDays { get; }
+}
