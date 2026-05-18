37f1162a0893b9618b2c224f5779b822a9a329fb
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-05-19 01:18:22 +0200

    feat(infrastructure): add DatabaseOptions class
    Added a new `DatabaseOptions` class in the `Lms.Auth.Infrastructure.Options` namespace.
    Defined a constant `SectionName` with the value `"ConnectionStrings"`.
    Introduced a non-nullable `AuthDb` property to hold the authentication database connection string.

---
+++ Lms.Auth/Lms.Auth.Infrastructure/Options/DataBaseOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace Lms.Auth.Infrastructure.Options;
+
+public class DatabaseOptions
+{
+    public const string SectionName = "ConnectionStrings";
+    public string AuthDb { get; set; } = null!;
+}
