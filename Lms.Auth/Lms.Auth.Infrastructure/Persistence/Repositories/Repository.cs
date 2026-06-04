29955e702595edc98738ceaf73430a1edc3307b0
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-06-05 00:31:35 +0200

    Update GetByIdAsync to use Guid instead of int
    Changed the GetByIdAsync method in the Repository class to accept a Guid parameter, updating the entity identifier type for database retrieval.

--- Lms.Auth/Lms.Auth.Infrastructure/Persistence/Repositories/Repository.cs
+++ Lms.Auth/Lms.Auth.Infrastructure/Persistence/Repositories/Repository.cs
@@ -19,7 +19,7 @@ public class Repository<T> : IRepository<T> where T : BaseEntity
         _dbSet = context.Set<T>();
     }
 
-    public async Task<T?> GetByIdAsync(int id) => await _dbSet.FindAsync(id);
+    public async Task<T?> GetByIdAsync(Guid id) => await _dbSet.FindAsync(id);
     public async Task<IEnumerable<T>> GetAllAsync() => await _dbSet.ToListAsync();
     public void Add(T entity) => _dbSet.Add(entity);
     public void Update(T entity) => _dbSet.Update(entity);
